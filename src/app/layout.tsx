a431c791fae485b91d9db75c362edb972d75f140
Programming-Seungwan <swsj4480@gmail.com>
2024-06-19 21:55:40 +0900

    Feat : set configurations of redux tool kit in nextJS
    1. redux를 nextJS 같은 SSR을 지원하는 프레임워크에서 사용하기 위해서는 기존의 전체 코드를 provider로 감싸주는 것이 좋지 않음
    2. store.ts에 configureStore()를 리턴하는 함수를 선언하고, provider를 클라이언트 컴포넌트로 만든 뒤 필요한 곳에서 사용하면 됨

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import StoreProvider from '@context/StoreProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-dvh">
-      <body className={`${inter.className} h-full`}>{children}</body>
+      <StoreProvider>
+        <body className={`${inter.className} h-full`}>{children}</body>
+      </StoreProvider>
     </html>
   );
 }
