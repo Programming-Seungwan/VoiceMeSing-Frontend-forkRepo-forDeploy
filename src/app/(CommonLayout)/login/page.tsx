e6d7d18c1b304703ff89b5f5746f8ae83fc647db
Programming-Seungwan <swsj4480@gmail.com>
2024-08-14 15:28:58 +0900

    Feat : 간편한 스타일링을 휘한 tailwind-merge, clsx 라이브러리 설치 및 관련 cn 유틸 함수 작성

--- src/app/(CommonLayout)/login/page.tsx
+++ src/app/(CommonLayout)/login/page.tsx
@@ -8,11 +8,8 @@ export default function LoginPage() {
   return (
     <main className="rightMain items-center overflow-y-scroll relative">
       <span className="normalFont text-[45px] mt-[70px]">LogIn Page</span>
-      {/* 이메일 패스워드 로그인 섹션 */}
       <EmailPasswordLoginSection />
-      {/* divider */}
       <Divider />
-      {/* sns 로그인 컴포넌트 */}
       <SocialLoginSection />
       <Footer />
     </main>
--- src/app/(CommonLayout)/page.tsx
+++ src/app/(CommonLayout)/page.tsx
@@ -2,7 +2,7 @@ import Footer from '@components/all/Footer/Footer';
 
 export default function HomePage() {
   return (
-    <main className="rightMain">
+    <main className="rightMain items-center overflow-y-scroll">
       <div className="h-[250px] flex justify-center items-center text-2xl tracking-[3px]">
         Train Your Own Voice and Create Cover!
       </div>
