1de54c95229c7ec519ec3fdb9725d19da492e2f1
Programming-Seungwan <swsj4480@gmail.com>
2024-08-14 15:38:13 +0900

    Feat : home 페이지 기초 퍼블리싱

--- src/app/(CommonLayout)/page.tsx
+++ src/app/(CommonLayout)/page.tsx
@@ -6,6 +6,22 @@ export default function HomePage() {
       <div className="h-[250px] flex justify-center items-center text-2xl tracking-[3px]">
         Train Your Own Voice and Create Cover!
       </div>
+
+      <div className="h-[450px] flex flex-col gap-y-5 normalFont">
+        <span className="font-[45px] text-white">STEP 1</span>
+        <span className="font-[30px] text-lightGray">
+          train AI Vocal providing your own voice
+        </span>
+        {/* 추후 사용 이미지가 들어갈 UI 위치 */}
+      </div>
+
+      <div className="h-[450px] flex flex-col gap-y-5 normalFont">
+        <span className='font-[45px] text-white"'>STEP 2</span>
+        <span className="font-[30px] text-lightGray">
+          Make AI Cover by using your voice model
+        </span>
+        {/* 추후 사용 이미지가 들어갈 UI 위치 */}
+      </div>
       <Footer />
     </main>
   );
