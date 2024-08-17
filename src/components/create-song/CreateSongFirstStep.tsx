f61f3b56dea6066a11132f355018e81850049668
Programming-Seungwan <swsj4480@gmail.com>
2024-08-17 15:14:51 +0900

    Feat : /create-song 페이지 2단계 UI 완성

--- src/components/create-song/CreateSongFirstStep.tsx
+++ src/components/create-song/CreateSongFirstStep.tsx
@@ -4,7 +4,7 @@ export default function CreateSongFirstStep() {
   return (
     <div className="w-[90%] mt-[50px] flex flex-col items-center gap-y-[60px]">
       <span className="text-[30px] text-white">Select your Voice Model</span>
-      <section className="w-[60%] h-[250px] flex justify-center items-center rounded-[20px] bg-[#222227]">
+      <section className="w-[60%] h-[250px] flex justify-center items-center rounded-[20px] bg-[#222227] hover:cursor-pointer">
         <MagnifyingGlassSVG />
       </section>
     </div>
--- src/components/create-song/CreateSongSecondStep.tsx
+++ src/components/create-song/CreateSongSecondStep.tsx
@@ -1,3 +1,15 @@
+import MicrophoneSVG from '@public/create-song/microphone.svg';
+import TipUI from '@components/all/UploadForm/TipUI';
+import VoiceUploadForm from '@components/train-vocal/VoiceUploadForm';
+
 export default function CreateSongSecondStep() {
-  return <div>second step</div>;
+  return (
+    <div className="w-[90%] mt-[50px] flex flex-col items-center">
+      <div className=" w-[80%] h-[70px] flex justify-center items-center gap-x-[45px] bg-[#303343] rounded-[30px]">
+        <MicrophoneSVG />
+        <span className="text-[20px] normalFont">Upload your Own Voice</span>
+      </div>
+      <VoiceUploadForm />
+    </div>
+  );
 }
