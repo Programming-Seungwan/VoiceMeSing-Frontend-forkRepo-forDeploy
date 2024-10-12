7766ecf4f3567fc0480b4e9cd98a4463ae6a9d4d
Programming-Seungwan <swsj4480@gmail.com>
2024-10-12 14:41:56 +0900

    Design : change wandookong logo

--- src/components/all/SideNavBar/Logo.tsx
+++ src/components/all/SideNavBar/Logo.tsx
@@ -1,5 +1,10 @@
-import GreenBeanSVG from '@public/SideNavBar/GreenBeanSVG.svg';
+// import GreenBeanSVG from '@public/SideNavBar/GreenBeanSVG.svg';
+import WandookongLogoSVG from '@public/SideNavBar/wandookongLogo.svg';
 
 export default function Logo() {
-  return <GreenBeanSVG className="w-[200px] h-[200px]" />;
+  return (
+    <div className="w-[200px] h-[200px] flex justify-center items-center">
+      <WandookongLogoSVG className=" " />
+    </div>
+  );
 }
