1535009200853d47fcae5bdfc212e5cde26a10b0
Programming-Seungwan <swsj4480@gmail.com>
2024-10-21 00:08:26 +0900

    Feat : /collection-model 페이지 백엔드 API 연결

--- src/app/(CommonLayout)/collections/model/page.tsx
+++ src/app/(CommonLayout)/collections/model/page.tsx
@@ -4,9 +4,44 @@ import VocalNameSearchInput from '@components/collections/model/VocalNameSearchI
 import CollectionVocalItemSection from '@components/collections/model/CollectionVocalItemSection';
 import useAccessTokenRedirect from '@hooks/useAccessTokenRedirect';
 import CollectionsModelSkeleton from '@components/collections/model/CollectionsModelSkeleton';
+import { useEffect, useState } from 'react';
+
+interface voiceModelProp {
+  voiceModelId: number;
+  voiceModelName: string;
+}
 
 export default function CollectionsModelPage() {
   const accessToken = useAccessTokenRedirect();
+  const [collectionModel, setCollectionModel] = useState(null);
+
+  useEffect(() => {
+    async function getUserColletionModels() {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN}/collection-model`,
+        {
+          method: 'GET',
+          credentials: 'include',
+          headers: {
+            access: `${accessToken}`,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(
+          '사용자의 collecion model 페이지의 음성 모델을 불러오는 데에 실패했습니다!'
+        );
+      }
+
+      const data = await response.json();
+
+      console.log(data);
+    }
+
+    getUserColletionModels();
+  }, [accessToken]);
+
   return (
     <>
       {accessToken === null ? (
