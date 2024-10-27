08a885ea4cd1ff8c176b8260f6b3ee22c754fb31
Programming-Seungwan <swsj4480@gmail.com>
2024-10-27 23:24:14 +0900

    Feat : make set key select UI in create-song page

--- src/components/create-song/CreateSongThirdStep.tsx
+++ src/components/create-song/CreateSongThirdStep.tsx
@@ -1,3 +1,19 @@
+import TipUI from '@components/all/UploadForm/TipUI';
+import PolygonSVG from '@public/create-song/Polygon.svg';
+
 export default function CreateSongThirdStep() {
-  return <div>third step</div>;
+  return (
+    <div className="w-[90%] mt-[85px] flex flex-col items-center">
+      <span className="text-[50px]  font-medium">Set Key</span>
+      <div className="w-[80%] flex flex-col gap-y-[15px] justify-center items-start mt-[40px]">
+        <TipUI />
+        <select
+          name="songKey"
+          className="w-full h-[40px] bg-backgroundNavy shadow-whiteShadow rounded-[10px] relative px-5"
+        >
+          <option value="auto">auto</option>
+        </select>
+      </div>
+    </div>
+  );
 }
