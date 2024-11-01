953a9c15b2bbf9e0a544e4d66f2dd871a0b89176
Programming-Seungwan <swsj4480@gmail.com>
2024-11-01 22:06:05 +0900

    Fix : Add margin bottom property on create-song first step UI

--- src/components/create-song/CreateSongFirstStep.tsx
+++ src/components/create-song/CreateSongFirstStep.tsx
@@ -13,7 +13,7 @@ export default function CreateSongFirstStep({
   }
 
   return (
-    <div className="w-[90%] mt-[50px] flex flex-col items-center gap-y-[60px]">
+    <div className="w-[90%] mt-[50px] mb-[70px] flex flex-col items-center gap-y-[60px]">
       <span className="text-[30px] text-white">Select your Voice Model</span>
       <section
         className="w-[60%] h-[250px] flex justify-center items-center rounded-[20px] bg-[#222227] hover:cursor-pointer"
