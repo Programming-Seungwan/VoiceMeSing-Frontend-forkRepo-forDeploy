bee3c7d01ad2a169ee7f677657f02fa0d1ee4f5a
Programming-Seungwan <swsj4480@gmail.com>
2024-11-05 16:00:28 +0900

    Feat : apply user nickname and password change API in profile page
    1. 기존의 nextJS router.refresh()는 서버에서의 새로운 데이터들을 가져오는 느낌이라 페이지 리프레시와는 살짝 다른 개념임

--- src/components/profile/ProfileForm.tsx
+++ src/components/profile/ProfileForm.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import EditPencilSVG from '@public/all/modelNameForm/editPencil.svg';
-import { ChangeEvent, Dispatch, SetStateAction } from 'react';
+import { ChangeEvent, Dispatch, SetStateAction, FormEvent } from 'react';
+import { useAppSelector } from '@hooks/reduxHooks';
+import { useRouter } from 'next/navigation';
 
 interface profileFormProp {
   nickname: string;
@@ -16,6 +18,12 @@ export default function ProfileForm({
   changeUserProfileNickname,
   changeUserProfilePassword,
 }: profileFormProp) {
+  const accessToken = useAppSelector(
+    (selector) => selector.accessToken.accessToken
+  );
+
+  const router = useRouter();
+
   const handleChangeUserNickname = (ev: ChangeEvent<HTMLInputElement>) => {
     changeUserProfileNickname(ev.target.value);
   };
@@ -24,8 +32,44 @@ export default function ProfileForm({
     changeUserProfilePassword(ev.target.value);
   };
 
+  // form이 제출되면 요청을 전송하고, 그 다음에 페이지를 새로고침 해주어야 한다.
+  const handleSubmitUserProfileForm = async (
+    ev: FormEvent<HTMLFormElement>
+  ) => {
+    ev.preventDefault();
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN}/profile-update`,
+        {
+          method: 'PATCH',
+          credentials: 'include',
+          headers: {
+            access: accessToken as string,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            nickname: nickname,
+            password: password,
+            introduction: 'user nickname and password change in profile page',
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('failed to patch new nickname and password of user!');
+      }
+
+      location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <form className="flex flex-col items-center grow">
+    <form
+      className="flex flex-col items-center grow"
+      onSubmit={handleSubmitUserProfileForm}
+    >
       <label htmlFor="nickname" className="w-full text-[20px] mb-[15px]">
         Nickname
       </label>
