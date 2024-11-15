6adb7bdd8b4a20ffbdd018eec4779793a0b81f7a
Programming-Seungwan <swsj4480@gmail.com>
2024-11-15 15:29:07 +0900

    Feat : make default flex layout of /community page

--- src/app/(CommonLayout)/community/page.tsx
+++ src/app/(CommonLayout)/community/page.tsx
@@ -1,9 +1,13 @@
 import Footer from '@components/all/Footer/Footer';
+import CommunityContentSections from '@components/community/CommunityContentSection';
 
 export default function Community() {
   return (
-    <main className="rightMain">
-      <div>This is Community page</div>
+    <main className="rightMain items-center overflow-y-scroll relative">
+      <div className="w-[90%] flex justify-start items-center mt-[50px]">
+        <span className="text-white fontNormal text-[35px]">Community</span>
+      </div>
+      <CommunityContentSections />
       <Footer />
     </main>
   );
